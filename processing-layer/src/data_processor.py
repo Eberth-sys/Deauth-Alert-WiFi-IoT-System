f611b9f028cc70229e8e148c324f1f1ebf2d49ad
HeyEberth <60485498+Eberth-sys@users.noreply.github.com>
2025-02-27 13:36:25 -0300

    style: No print for save log
    - Elimino el mensaje de log guardo

--- processing-layer/src/data_processor.py
+++ processing-layer/src/data_processor.py
@@ -4,4 +4,4 @@ def process_data(device_name, data, timestamp):
     log_entry = f"[{timestamp}] {device_name}: {data}\n"
     with open('logs/ble_data.log', 'a') as log_file:
         log_file.write(log_entry)
-    print(f"[SAVED] Datos guardados en el log.")
+    # print(f"[SAVED] Datos guardados en el log.")
