30745d3a04389bab873eacc76448d7f0a8b79214
HeyEberth <60485498+Eberth-sys@users.noreply.github.com>
2025-03-07 17:38:29 -0300

    fix(log): Elimino duplicación de timestamps en logs de BLEManager
    - Se eliminó la fecha en los "print()" para evitar duplicaciones en los archivos logs.

--- processing-layer/src/ble_manager.py
+++ processing-layer/src/ble_manager.py
@@ -1,4 +1,5 @@
 #src/ble_manager.py
+
 from bleak import BleakClient, BleakScanner  # Librería para gestionar conexiones BLE
 import asyncio  # Para manejar tareas asíncronas
 import logging  # Librería para gestionar logs en un archivo
@@ -23,8 +24,7 @@ class BLEManager:
     async def handle_device(self, device):  # Manejo de cada ESP32
         while True:  # Bucle infinito para intentar la conexión de los ESP32
             try:
-                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                msg = f"[INFO] [{timestamp}] Buscando {device['name']} ({device['address']})..."
+                msg = f"[INFO] Buscando {device['name']} ({device['address']})..."
                 print(msg)
                 logging.info(msg)  # Guardar el mensaje en el archivo de logs
 
@@ -33,47 +33,45 @@ class BLEManager:
 
                 if not found_device:
                     if self.device_status[device["address"]]:  # Solo mostrar si estaba conectado antes
-                        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                        msg = f"[DISCONNECTED]  [{timestamp}] {device['name']} se ha desconectado."
+                        msg = f"[DISCONNECTED] {device['name']} se ha desconectado."
                         print(msg)
                         logging.info(msg)  # Guardar el mensaje en los logs
                         self.device_status[device["address"]] = False  # Marcar como desconectado
 
-                    msg = f"[WARNING] [{timestamp}] {device['name']} no encontrado. Reintentando en 10 segundos..."
+                    msg = f"[WARNING] {device['name']} no encontrado. Reintentando en 10 segundos..."
                     print(msg)
                     logging.warning(msg)  # Guardar en logs con nivel WARNING
                     await asyncio.sleep(10)  # Esperar 10 segundos antes de reintentar
                     continue  # Continuar buscando el dispositivo
 
-                msg = f"[INFO] [{timestamp}] Intentando conectar a {device['name']}..."
+                msg = f"[INFO] Intentando conectar a {device['name']}..."
                 print(msg)
                 logging.info(msg)
 
                 async with BleakClient(device["address"]) as client:
                     if client.is_connected:
                         self.device_status[device["address"]] = True  # Marcar como conectado
-                        msg = f"[CONNECTED] ✅ [{timestamp}] Conectado a {device['name']}"
+                        msg = f"[CONNECTED] ✅ Conectado a {device['name']}"
                         print(msg)
                         logging.info(msg)
 
                         try:
                             await client.pair(protection_level=2)  # Intentar cifrar la conexión
-                            msg = f"[SECURE] 🔒 [{timestamp}] Conexión cifrada con {device['name']}"
+                            msg = f"[SECURE] 🔒 Conexión cifrada con {device['name']}"
                             print(msg)
                             logging.info(msg)
                         except Exception as e:
-                            msg = f"[WARNING] [{timestamp}] No se pudo cifrar la conexión: {e}"
+                            msg = f"[WARNING] No se pudo cifrar la conexión: {e}"
                             print(msg)
                             logging.warning(msg)
 
                         # Manejo de notificaciones BLE
                         def notification_handler(sender, data):
-                            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Fecha y hora del evento registrado
                             decoded_data = data.decode('utf-8')  # Convertir datos BLE en texto legible
 
                             # Filtrar mensajes de datos (No guardarlos en logs, ya están en la BD)
                             if "[ALERT]" in decoded_data:
-                                msg = f"[DATA] [{timestamp}] {device['name']}: {decoded_data}"
+                                msg = f"[DATA] {device['name']}: {decoded_data}"
                                 print(msg)
 
                                 try:
@@ -91,12 +89,12 @@ class BLEManager:
                                     guardar_alerta(nodo_iot, spoofed_bssid, target_mac, bssid, canal)
 
                                 except Exception as e:
-                                    msg = f"[ERROR] [{timestamp}] Error al procesar los datos BLE: {e}"
+                                    msg = f"[ERROR] Error al procesar los datos BLE: {e}"
                                     print(msg)
                                     logging.error(msg)  # Guardar error en logs
 
                         await client.start_notify(self.characteristic_uuid, notification_handler)
-                        msg = f"[INFO] [{timestamp}] Esperando datos de {device['name']}..."
+                        msg = f"[INFO] Esperando datos de {device['name']}..."
                         print(msg)
                         logging.info(msg)
 
@@ -104,24 +102,23 @@ class BLEManager:
                             await asyncio.sleep(1)
 
             except Exception as e:
-                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 error_message = str(e)
 
                 if "org.bluez.Error.InProgress" in error_message:
-                    msg = f"[ERROR] [{timestamp}] BlueZ ocupado. Esperando 10 segundos antes de reintentar..."
+                    msg = f"[ERROR] BlueZ ocupado. Esperando 10 segundos antes de reintentar..."
                     print(msg)
                     logging.error(msg)
                     await asyncio.sleep(10)
                 elif "Characteristic" in error_message:
-                    msg = f"[ERROR] [{timestamp}] Error con {device['name']}: No se pudo acceder a la característica."
+                    msg = f"[ERROR] Error con {device['name']}: No se pudo acceder a la característica."
                     print(msg)
                     logging.error(msg)
                 else:
-                    msg = f"[ERROR] [{timestamp}] Error con {device['name']}: {error_message}"
+                    msg = f"[ERROR] Error con {device['name']}: {error_message}"
                     print(msg)
                     logging.error(msg)
 
-                msg = f"[DISCONNECTED] [{timestamp}] Reintentando conexión con {device['name']} en 10 segundos..."
+                msg = f"[DISCONNECTED] Reintentando conexión con {device['name']} en 10 segundos..."
                 print(msg)
                 logging.info(msg)
                 await asyncio.sleep(10)
